d5b2f9bb5c756b65c7a658949bad5d7904aa281e
seth-jg <65782027+seth-jg@users.noreply.github.com>
2023-11-23 14:32:57 +0000

    Update CoffeeaApp.py

--- CoffeeaApp.py
+++ CoffeeaApp.py
@@ -1,12 +1,17 @@
-import tkinter as tk
-from tkinter import ttk
+import sqlite3
+from tkinter import *
+from tkinter.ttk import *
 from tkinter import font
 
+def remove_all_widgets(parent):
+    for widget in parent.winfo_children():
+        widget.destroy()
+
 def logIn():
+    remove_all_widgets(root)
     return
 
 def register():
-    print()
     return
 
 def storeLocator():
@@ -28,16 +33,25 @@ def orderedScreen():
     return
 
 # Create the main window
-root = tk.Tk()
-root.geometry("3000x500")
-root.config(bg="#D2B48C")
-
+root = Tk()
+root.geometry("300x500")
+root.resizable(False, False)
+# Background
+background = "#D2B48C" # Color
+root.config(bg=background) # Change root
+s = Style() # ttk frame
+s.configure("TFrame", background="blue") # Set color of ttk frame
 custom_font = font.Font(family="Arial", size=22, weight="bold")
 
-logo = ttk.Label(root, text="Beans and Brew Cafe", font=custom_font)
-logo.pack(fill="x")  # Center the label text
+logo = Label(root, text="Beans and Brew Cafe", font=custom_font, background="red")
+logo.place(x=0, y=0, height=50)
 
-# Add other widgets and components to your app
+mainFrame = Frame(root, style="TFrame")
+mainFrame.place(width=300, height=400, x=0, y=50)
+
+openButton = Button(mainFrame, text="Login", command=logIn)
+openButton.place(width=100, height=100, x=100, y=150)
+
 
 # Run the Tkinter event loop
 root.mainloop()
