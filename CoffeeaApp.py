9271d199b4bf64ebd991171a29e1e309246dc71c
seth-jg <65782027+seth-jg@users.noreply.github.com>
2023-11-23 11:38:29 +0000

    base

---
+++ CoffeeaApp.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+
+def logIn():
+    return
+
+def register():
+    return
+
+def storeLocator():
+    return
+
+def home():
+    return
+
+def profile():
+    return
+
+def basket():
+    return
+
+def checkout():
+    return
+
+def orderedScreen():
+    return
+
+# Create the main window
+root = tk.Tk()
+root.geometry("3000x500")
+root.config(bg="#D2B48C")
+
+custom_font = font.Font(family="Arial", size=22, weight="bold")
+
+logo = ttk.Label(root, text="Beans and Brew Cafe", font=custom_font)
+logo.pack(fill="x")  # Center the label text
+
+# Add other widgets and components to your app
+
+# Run the Tkinter event loop
+root.mainloop()
---
+++ deCommands.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+def createTable():
+    return
+
+def insertData():
+    return
+
+def veiwData():
+    return
+
+def updateData():
+    return
